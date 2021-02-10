e516811a2ab3b08fa75dd0a5d194d59fcf480414
Artur <info@aigroupusa.com>
2021-02-10 15:46:22 -0500

    commit tree f5ac69f0f0d79660a297b209c5af9d03dba1d952

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,45 @@
+#require 'pry'
 class Transfer
-  # your code here
+  attr_accessor :sender, :receiver, :amount, :status
+
+  def initialize (sender, receiver, amount, status = "pending")
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = status
+  end
+
+  def valid?
+    if sender.valid? == false || receiver.valid? == false
+      false
+      else
+        true
+      end
+  end
+
+  def execute_transaction 
+    #binding.pry
+    if self.valid? == false || sender.balance < amount 
+      self.status = "rejected"
+      "Transaction rejected. Please check your account balance."
+    elsif self.status == "complete"
+      return
+    else 
+      sender.balance -= amount
+      receiver.balance += amount
+      self.status = "complete"
+    end
+  end
+
+    def reverse_transfer
+      #binding.pry
+      if self.status == "complete"
+        sender.balance += self.amount
+      receiver.balance -= self.amount
+      self.status = "reversed" 
+        
+      end
+      
+    end
+
 end
